7143b259b471d834736116dfb5f9cc2088744f70
Giovanni <giova.bifulco@gmail.com>
2024-03-25 14:37:33 +0100

    Initial Commit

---
+++ SurelyNoTelegram/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SurelyNoTelegram
+//
+//  Created by Giovanni Bifulco on 25/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SurelyNoTelegram/SurelyNoTelegramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurelyNoTelegramApp.swift
+//  SurelyNoTelegram
+//
+//  Created by Giovanni Bifulco on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SurelyNoTelegramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
